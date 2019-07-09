c2a643234764ff994819be090e86323b5e548aea
Domingo Espinosa <domingo.espinosa@codurance.com>
2019-07-09 16:44:13 +0200

    First test wanted criminals array

--- src/main/java/Bulletin.java
+++ src/main/java/Bulletin.java
@@ -1,13 +1,24 @@
 public class Bulletin {
-    private String updates;
+    private String[] wantedCriminals;
 
     public Bulletin(String updates) {
-
-        this.updates = updates;
+        parseBulletinCriminals(updates);
     }
 
 
-    public String getWantedCriminals() {
-        return "Hubert Popovic";
+    public String[] getWantedCriminals() {
+        System.out.println(this.wantedCriminals);
+        return this.wantedCriminals;
+    }
+
+    private void parseBulletinCriminals(String updates) {
+        String[] names = new String[1];
+        if (updates.contains(",")){
+            names = updates.split(",");
+        }
+        if (updates.contains(":")){
+            names[0] = updates.substring(updates.indexOf(":")+1).trim();
+        }
+        this.wantedCriminals = names;
     }
 }
--- src/test/java/BulletinShould.java
+++ src/test/java/BulletinShould.java
@@ -1,14 +1,30 @@
+import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class BulletinShould {
+
+    private String[] wantedCriminals;
+
     @Test
     public void update_currently_wanted_criminal() {
         Bulletin bulletin = new Bulletin("Wanted by the State: Hubert Popovic");
-        assertThat(bulletin.getWantedCriminals(), is("Hubert Popovic"));
+        wantedCriminals = bulletin.getWantedCriminals();
+        assertThat(wantedCriminals[0], is("Hubert Popovic"));
     }
 
+    @Test
+    public void update_currently_wanted_criminals() {
+        Bulletin bulletin = new Bulletin("Wanted by the State: Hubert Popovic, Igor Fried");
+        ArrayList<String> wantedCriminals = new ArrayList();
+        wantedCriminals.add("Hubert Popovic");
+        wantedCriminals.add("Igor Fried");
+        assertThat(bulletin.getWantedCriminals().length, is(wantedCriminals.size()));
+    }
 
 }
