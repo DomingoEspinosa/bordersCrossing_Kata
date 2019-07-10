74ee1877ba4700a7aa77f82495990013958c69af
Domingo Espinosa <domingo.espinosa@codurance.com>
2019-07-10 11:59:25 +0200

    Vaccination in green

--- src/main/java/Bulletin.java
+++ src/main/java/Bulletin.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bulletin {
     private String[] wantedCriminals;
+    private Map<String, ArrayList<String>> requiredVaccinationsForCountries = new HashMap<String, ArrayList<String>>();
 
     public Bulletin(String updates) {
         parseBulletinCriminals(updates);
@@ -17,7 +22,24 @@ public class Bulletin {
         if (containsOnlyOneCriminal(updates)) {
             names[0] = updates.substring(updates.indexOf(':') + 1).trim();
         }
+        if (containsVaccinationWord(updates)){
+            String vacination = updates.substring(updates.lastIndexOf("require") + 7, updates.indexOf("vaccination")).trim();
+            ArrayList<String> vaccinations = new ArrayList<String>();
+            vaccinations.add(vacination);
+
+            String countries = updates.substring(updates.indexOf("of")+2, updates.indexOf("require")).trim();
+            String[] listCountries = countries.split(",");
+
+            for (String country: listCountries){
+                requiredVaccinationsForCountries.put(country, vaccinations);
+            }
+
+        }
         this.wantedCriminals = names;
+    }
+
+    private boolean containsVaccinationWord(String updates) {
+        return updates.contains("vaccination");
     }
 
     private boolean containsMoreThanOneCriminal(String updates) {
@@ -26,5 +48,9 @@ public class Bulletin {
 
     private boolean containsOnlyOneCriminal(String updates) {
         return updates.contains(":");
+    }
+
+    public ArrayList<String> getRequiredVaccinationsFor(String country) {
+        return requiredVaccinationsForCountries.get(country);
     }
 }
--- src/test/java/BulletinShould.java
+++ src/test/java/BulletinShould.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.*;
 import static org.hamcrest.core.Is.is;
 
 public class BulletinShould {
@@ -12,17 +16,50 @@ public class BulletinShould {
     @Test
     public void update_currently_wanted_criminal() {
         Bulletin bulletin = new Bulletin("Wanted by the State: Hubert Popovic");
+
         wantedCriminals = bulletin.getWantedCriminals();
+
         assertThat(wantedCriminals[0], is("Hubert Popovic"));
     }
 
     @Test
     public void update_currently_wanted_criminals() {
         Bulletin bulletin = new Bulletin("Wanted by the State: Hubert Popovic, Igor Fried");
+
         ArrayList<String> wantedCriminals = new ArrayList();
         wantedCriminals.add("Hubert Popovic");
         wantedCriminals.add("Igor Fried");
+
         assertThat(bulletin.getWantedCriminals().length, is(wantedCriminals.size()));
+    }
+
+    @Test
+    public void update_required_vaccinations() {
+        Bulletin bulletin = new Bulletin("Citizens of Antegria, Republia, Obristan require polio vaccination");
+
+        Map<String, ArrayList<String>> countryToNecessaryVaccination = new HashMap<String, ArrayList<String>>();
+        ArrayList<String> vaccinations = new ArrayList<String>();
+        vaccinations.add("polio");
+        countryToNecessaryVaccination.put("Antegria", vaccinations);
+        countryToNecessaryVaccination.put("Republia", vaccinations);
+        countryToNecessaryVaccination.put("Obristan", vaccinations);
+
+        assertThat(countryToNecessaryVaccination.get("Antegria").get(0), is(bulletin.getRequiredVaccinationsFor("Antegria").get(0)));
+
+    }
+
+    @Test
+    public void update_required_vaccinations_for_rubeola() {
+        Bulletin bulletin = new Bulletin("Citizens of Antegria, Republia, Obristan require Rubeola vaccination");
+
+        Map<String, ArrayList<String>> countryToNecessaryVaccination = new HashMap<String, ArrayList<String>>();
+        ArrayList<String> vaccinations = new ArrayList<String>();
+        vaccinations.add("Rubeola");
+        countryToNecessaryVaccination.put("Antegria", vaccinations);
+        countryToNecessaryVaccination.put("Republia", vaccinations);
+        countryToNecessaryVaccination.put("Obristan", vaccinations);
+
+        assertThat(countryToNecessaryVaccination.get("Antegria").get(0), is(bulletin.getRequiredVaccinationsFor("Antegria").get(0)));
     }
 
 }
